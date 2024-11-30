e83b61c9bfe1e43d8ed643e3568cd5adafa9e8e4
nayem73 <nayem73@gmail.com>
2024-11-30 18:34:02 +0600

    included e-grid icon

--- frontend/road-damage-classification/src/App.js
+++ frontend/road-damage-classification/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import UploadForm from './components/UploadForm';
 import './App.css';
+import logo from './assets/egrid-logo.svg';
 
 function App() {
     return (
         <div className="App">
-            <header>
+            <header className="app-header">
+                <img src={logo} alt="E-Grid Logo" className="logo" />
                 <h1>Road Damage Analysis</h1>
             </header>
             <main>
